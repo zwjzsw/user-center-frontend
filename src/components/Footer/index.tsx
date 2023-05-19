e03f3066959dc280fcc7397b1fede88633507258
朱伟杰 <zhuweijie.sh@chinaccs.cn>
2023-05-19 21:39:42 +0800

    前端

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import {PLANET_LINK} from "@/constants";
+const Footer: React.FC = () => {
+  const defaultMessage = '鱼皮出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'planet',
+          title: '知识星球',
+          href: PLANET_LINK,
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <><GithubOutlined /> 鱼皮 Github</>,
+          href: 'https://github.com/liyupi',
+          blankTarget: true,
+        },
+        {
+          key: 'codeNav',
+          title: '编程导航',
+          href: 'https://www.code-nav.cn',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
